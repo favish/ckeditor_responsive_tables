e1974fbce66b91e03afb286e0aaeb5ea3894f388
travisczerw <travis@favish.com>
2016-02-23 13:08:58 -0600

    feat(tablesaw UI options): added ability to toggle 2 different table modes (sortable and swipe) from dialog UI

--- plugins/responsive_tables/dialogs/responsive_tables.js
+++ plugins/responsive_tables/dialogs/responsive_tables.js
@@ -25,12 +25,35 @@ CKEDITOR.dialog.add( 'tableDialog', function ( editor ) {
       },
       {
         id: 'tab-adv',
-        label: 'Options',
+        label: 'Table Options',
         elements: [
           {
+            type: 'radio',
+            id: 'tableModes',
+            label: 'Advanced Table Modes',//Makes the table sortable or a swipe table
+            items: [ ['Sortable'], ['Swipe'] ]
+          },
+          {
             type: 'text',
-            id: 'id',
-            label: 'Id'
+            id: 'evenColor',
+            label: 'Even Row Color',
+            default: '#ffffff'//from their CSS, their base even zebra color is just white
+          },
+          {
+            type: 'text',
+            id: 'oddColor',
+            label: 'Odd Row Color',
+            default: '#FEF6E8'//from their CSS, this is the zebra odd row color
+          },
+          {
+            type: 'text',
+            id: 'headerColor',
+            label: 'Table Header Color'//Sets the table header color
+          },
+          {
+            type: 'text',
+            id: 'fontColor',
+            label: 'Font Color'//Sets all font color in the table
           }
         ]
       }
@@ -40,13 +63,27 @@ CKEDITOR.dialog.add( 'tableDialog', function ( editor ) {
       var table = editor.document.createElement( 'table' );
       var rows = dialog.getValueOf('tab-basic', 'rows');
       var columns = dialog.getValueOf('tab-basic', 'columns');
+      var advancedTableMode = dialog.getValueOf('tab-adv', 'tableModes');
 
       var thead = new CKEDITOR.dom.element('thead');
       var tbody = new CKEDITOR.dom.element('tbody');
 
-      //Set classes and data-attributes that Tablesaw library requires
-      table.addClass('cbs-table tablesaw tablesaw-stack');
-      table.data('tablesaw-mode', 'stack');
+      //Set classes and data-attributes that Tablesaw library requires based on UI options. Default is Stack table
+      switch(advancedTableMode) {
+        case 'Sortable':
+          table.addClass('cbs-table tablesaw tablesaw-stack tablesaw-sortable');
+          table.data('tablesaw-sortable', '');
+          table.data('tablesaw-sortable-switch', '');
+          break;
+        case 'Swipe':
+          table.addClass('cbs-table tablesaw tablesaw-swipe');
+          table.data('tablesaw-mode', 'swipe');
+          break;
+        default:
+          table.addClass('cbs-table tablesaw tablesaw-stack');
+          table.data('tablesaw-mode', 'stack');
+      }
+
       thead.appendTo(table);
       tbody.appendTo(table);
 
@@ -63,6 +100,11 @@ CKEDITOR.dialog.add( 'tableDialog', function ( editor ) {
         for(var x=0; x < columns; x++) {
           if(y == 0) {
             var th = new CKEDITOR.dom.element('th');
+            //If sortable option selected, add this data-attribute to headers to enable them to be sorted
+            if(advancedTableMode === 'Sortable') {
+              th.data('tablesaw-sortable-col', '');
+            }
+
             th.appendTo(tr);
           }
           else {
